1ab98dc0d5ab811e094a0ad1a729bac508eb756a
Akhila <90416008+ry-aki@users.noreply.github.com>
2023-08-31 00:01:12 +0530

    Updated widgets.py

--- widgets.py
+++ widgets.py
@@ -3,3 +3,24 @@ st.title("WIDGETS")
 
 if st.button("Subcribe"):
     st.write("Subscribed!!")
+
+name = st.text_input("Name")
+st.write(name)
+
+address = st.text_area("Address")
+st.write(address)
+
+date = st.date_input("Enter date")
+time = st.time_input("Enter time")
+
+if st.checkbox("Accept the terms", value = False):
+    st.write("THANK YOU!!")
+
+
+st.radio("Colours", ['r','b','g'], index = 0)
+st.selectbox("Colours", ['r','b','g'], index = 0)
+
+st.multiselect("Colours", ['r','b','g'])
+st.slider("age", min_value = 18, max_value = 60, value = 30, step = 5)
+st.number_input("age", min_value = 18, max_value = 60, value = 30, step = 5)
+st.file_uploader("Upload a file")
